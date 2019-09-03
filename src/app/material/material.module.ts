d4b968464e087c73938ac8100c59442c5d3a6e2d
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-09-03 13:22:09 +0200

    reactive form set up with arrays

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,9 +4,24 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatChipsModule } from '@angular/material/chips'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 const MaterialComponents =  [
-  MatIconModule, MatListModule, MatButtonModule, MatSidenavModule
+  MatIconModule, 
+  MatListModule, 
+  MatButtonModule, 
+  MatSidenavModule, 
+  MatInputModule, 
+  MatSelectModule, 
+  MatCheckboxModule,
+  MatChipsModule,
+  FormsModule,
+  ReactiveFormsModule
 ]
 
 @NgModule({
--- src/app/recipes/new-recipe/new-recipe.component.ts
+++ src/app/recipes/new-recipe/new-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms'
 
 @Component({
   selector: 'new-recipe',
@@ -6,10 +7,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-recipe.component.scss']
 })
 export class NewRecipeComponent implements OnInit {
+  // ts
+  myForm: FormGroup;
 
-  constructor() { }
+  //private publie = service
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.myForm = this.fb.group({
+      title: '',
+      ingredients: this.fb.array([]),
+      method: this.fb.array([]),
+      notes: this.fb.array([])
+    })
+
+  //subscribe = observable rxjs
+  this.myForm.valueChanges.subscribe(console.log)
+
+  }
+  //getter
+  //INGREDIENTS
+  get ingredientForms() {
+    return this.myForm.get('ingredients') as FormArray
   }
 
+  addIngredient() {
+    const ingredient = this.fb.group ({
+      name: [],
+      amount: [],
+    })
+    this.ingredientForms.push(ingredient)
+  }
+
+  deleteIngredient(i) {
+    this.ingredientForms.removeAt(i)
+  }
+
+  //METHODS
+  get methodForms() {
+    return this.myForm.get('method') as FormArray
+  }
+
+  addMethod() {
+    const method = this.fb.group ({
+      step: []
+    })
+    this.methodForms.push(method)
+  }
+
+  deleteMethod(i) {
+    this.methodForms.removeAt(i)
+  }
+
+  //Notes
+  get notesForms() {
+    return this.myForm.get('notes') as FormArray
+  }
+
+  addNote() {
+    const note = this.fb.group ({
+      sideNote: []
+    })
+    this.notesForms.push(note)
+  }
+
+  deleteNote(i) {
+    this.notesForms.removeAt(i)
+  }
 }
